df661d937049a1bd12255832a6d96171cb910d04
timiwritescode <odebodezion@gmail.com>
2025-03-01 10:45:21 +0100

    feat: add method to read file to string

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,6 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 class Util {
-    static String readFileToString(String filepath) {
-        return null;
+    static String readFileToString(String filepath) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filepath))) {
+            String currentLine = reader.readLine();
+
+            while (currentLine != null) {
+                stringBuilder.append(currentLine);
+                stringBuilder.append("\n");
+                currentLine = reader.readLine();
+            }
+
+            if (!stringBuilder.isEmpty()) {
+                stringBuilder.setLength(stringBuilder.length() - 1);
+            }
+        }
+        return stringBuilder.toString();
     }
 
     static void writeStringToFile(String value, String filename, String pathToNewFile) {
@@ -8,6 +27,7 @@ class Util {
         // if pathToNewFile is not defined
         // name should default to .env.example is not provided
         // it should reject .env as a name
+
     }
 
 }
