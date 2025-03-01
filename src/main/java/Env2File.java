4b1c602292bedfbffaf87f93b2cb879b49fee6f1
timiwritescode <odebodezion@gmail.com>
2025-03-01 02:51:31 +0100

    init: setup env2file utilities for .env files processing

--- src/main/java/Env2File.java
+++ src/main/java/Env2File.java
@@ -1,1 +1,16 @@
-class Env2File {}
+import java.util.List;
+
+class Env2File {
+
+    private List<String> retrieveKeysFromEnvFile(String filepath) {
+        // an environment key pair is defined as that which follows the regexp
+        // [0-9][A-Za-Z]s+=[0-9][A-Za-Z]s
+        String fileContent = Util.readFileToString(filepath);
+        return null;
+    }
+
+    public static void writeEnvKeysToAFile(String[] keys) {
+
+    }
+
+}
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,3 +1,13 @@
 class Util {
+    static String readFileToString(String filepath) {
+        return null;
+    }
+
+    static void writeStringToFile(String value, String filename, String pathToNewFile) {
+        // should default to current working directory
+        // if pathToNewFile is not defined
+        // name should default to .env.example is not provided
+        // it should reject .env as a name
+    }
 
 }
