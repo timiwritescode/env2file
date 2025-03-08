1840d420977c59f9e33f51fd6516dac35dc6e66a
timiwritescode <odebodezion@gmail.com>
2025-03-09 00:02:53 +0100

    feat: integrate PicoCLI for CLI argument parsing in GraalVM binary
    - Implemented PicoCLI to handle command-line arguments for the tool.
    - Enabled building a native executable using GraalVM.
    - Improved command-line usability and flexibility.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,16 +13,33 @@ public class Main implements Runnable {
     @CommandLine.Parameters(index = "0", description = "Path to file")
     private String filepath;
 
-    @CommandLine.Parameters(index = "1", description = "Path to output file")
+    @CommandLine.Parameters(index = "1", description = "Path to output file", arity ="0..1")
     private String filename;
+
+    @CommandLine.Option(
+            names = {"-g", "--git"},
+            description = "Enables the automatic git option for env2file",
+            required = false
+    )
+    private boolean gitOption;
 
     @Override
     public void run() {
-        try {
-            Env2File env2File = new Env2File(filepath);
-            env2File.writeEnvKeysToAFile("", filename);
 
-        } catch (IOException e) {
+        try {
+
+            Env2File env2File = new Env2File(filepath);
+
+            if (gitOption) {
+                String outputfile = filename == null ? ".env.sample.txt" : filename;
+                env2File.saveEnvironmentKeysToGit(outputfile);
+
+            } else {
+                env2File.writeEnvKeysToAFile("", filename);
+            }
+
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
